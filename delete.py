a0523ef7dce1a304ace54ebc3b711a6b65a8b34c
Ezra Kim <ezra.t.kim@gmail.com>
2021-01-04 19:32:59 -0800

    Added delete.py, upload.py, requirements.txt, testfile

---
+++ delete.py
@@ -0,0 +1,23 @@
+import boto3
+import sys
+from botocore.exceptions import NoCredentialsError
+
+
+def delete_from_aws(bucket, s3_object):
+    s3 = boto3.client('s3')
+
+    try:
+        s3.delete_object(
+            Bucket=bucket, 
+            Key=s3_object)
+        print("Delete Successful")
+        return True
+    except FileNotFoundError:
+        print("The file was not found")
+        return False
+    except NoCredentialsError:
+        print("Credentials not available")
+        return False
+
+
+deleted = delete_from_aws(sys.argv[1], sys.argv[2])
