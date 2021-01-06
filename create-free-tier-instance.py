6367aa9db886dd4651c2c979c944f2e7e3a67665
Ezra Kim <ezra.t.kim@gmail.com>
2021-01-05 20:29:20 -0800

    Updated: create-free-tier-instance.py. Created webapp.py

--- create-free-tier-instance.py
+++ create-free-tier-instance.py
@@ -1,13 +1,19 @@
 import boto3
+import sys
 
 def create_free_tier_instance():
     ec2 = boto3.resource('ec2')
+
+    response = ec2.create_key_pair(KeyName='test-key-pair')
+    with open('test-key-pair.txt', 'w') as f:
+        print(response.key_material, file=f)
 
     instances = ec2.create_instances(
         ImageId='ami-0be2609ba883822ec',
         MinCount=1,
         MaxCount=1,
-        InstanceType='t2.micro'
+        InstanceType='t2.micro',
+        KeyName=response.key_name
     )
     print ("Instance Created")
 
